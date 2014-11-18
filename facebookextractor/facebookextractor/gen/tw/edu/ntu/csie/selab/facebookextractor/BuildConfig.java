b2757f6eff9def07ca2bd6b746f1006ad2da20e7
Terence <terenzio@gmail.com>
2014-11-18 15:09:18 +0800

    deleted FB extractor

--- facebookextractor/facebookextractor/gen/tw/edu/ntu/csie/selab/facebookextractor/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package tw.edu.ntu.csie.selab.facebookextractor;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
